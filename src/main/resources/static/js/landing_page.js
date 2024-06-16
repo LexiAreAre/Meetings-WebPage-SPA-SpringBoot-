f9ec3db22ebed532467f17403815037b2a1c9f18
Alexander Ramírez Calderón <alexram999rc@gmail.com>
2024-06-16 14:16:11 -0600

    Stable version of sections.

--- src/main/resources/static/js/landing_page.js
+++ src/main/resources/static/js/landing_page.js
@@ -16,11 +16,30 @@ function show_landing_page() {
                 <li><a href="#">Postres</a></li>
             </ul>
         </div>
-        <div class="flex-item">
+        <div class="flex-item" id="flex-item-custom">
             <!-- Content for section 2 goes here -->
+            <div class="container text-center">
+                <div>
+                    <h1 class="section-subtitle">Menú</h1>
+                </div>
+            <br>
+                 <div class="section-content">
+                    <p>Seleccione una CATEGORÍA en el panel de la izquierda para mostrar sus deliciosos platillos</p>
+                    <br>
+<!--                    insert Menu.png-->
+                    <img src="images/Menu.png" alt="Menu" class="menu"> 
+                 </div>
+            </div>
+            
         </div>
-        <div class="flex-item">
+        <div class="flex-item" id="flex-item-custom">
             <!-- Content for section 3 goes here -->
+            <div class="container text-center">
+                <div>
+                    <h1 class="section-subtitle">Orden</h1>
+                </div>
+            <br>
+            </div>
         </div>
     </div>
     `
